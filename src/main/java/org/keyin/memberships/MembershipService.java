9b36327f9bc13c62f8d386b28949e2dcdb52639b
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2025-07-31 12:23:30 -0230

    added packages for logger, and gym product to match summer doc

--- src/main/java/org/keyin/memberships/MembershipService.java
+++ src/main/java/org/keyin/memberships/MembershipService.java
@@ -1,13 +1,16 @@
 package org.keyin.memberships;
 
-
-// Service class for membership handle all the business logic
-// and only uses the DAO to interact with the database it does not have methods to do so
-// you can inject in your dao to use in your service. An example will be in the code
+/**
+ * Service class responsible for handling all business logic related to memberships.
+ * This class interacts with the MembershipDAO to perform database operations.
+ * The DAO should be injected into this service to access its methods.
+ * Example usage:
+ *   MembershipDAO dao = new MembershipDAO();
+ *   // Use dao within service methods
+ */
 public class MembershipService {
 
-    // When you inject in the DAO you have access to all methods in it
-    //MembershipDAO dao = new MembershipDAO();
-
+    // Inject the MembershipDAO to access its database methods
+    // MembershipDAO dao = new MembershipDAO();
 
 }
