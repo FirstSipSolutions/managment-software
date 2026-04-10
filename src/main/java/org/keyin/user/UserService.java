95a48aa57604631c1977638cbef299dc0a6be53a
Justin Seaward <jut_seaward@yahoo.com>
2026-04-10 15:27:51 -0230

    UserService: added new UserDAO variable, added constructors, added method to loginForUser() and a mother to addUser that is empty.

--- src/main/java/org/keyin/user/UserService.java
+++ src/main/java/org/keyin/user/UserService.java
@@ -1,4 +1,26 @@
 package org.keyin.user;
 
+import java.sql.SQLException;
+
 public class UserService {
+
+    private UserDAO userDAO = new UserDAO();
+
+    public UserService(){
+    }
+
+    public UserService(String username, String password) throws SQLException {
+
+    }
+
+    public User loginForUser(String username, String password) throws SQLException{
+        User user = userDAO.getUserByUsername(username);
+        if (user != null && user.getUser_password().equals(password)) {
+            return user;
+        }
+        return null;
+    }
+
+    public void addUser(User user) throws SQLException{
+    }
 }
