6268bf4f1c33c54236cebc1fc0de45f8d9757b4c
Justin Seaward <jut_seaward@yahoo.com>
2026-04-13 14:37:30 -0230

    ServicePlanService: Added ServiceDAO object.

--- src/main/java/org/keyin/serviceplans/ServicePlanService.java
+++ src/main/java/org/keyin/serviceplans/ServicePlanService.java
@@ -1,7 +1,7 @@
 package org.keyin.serviceplans;
 
 /**
- * Service class responsible for handling all business logic related to service plans.
+ * Service class is responsible for handling all business logic related to service plans.
  * This class interacts with the ServiceDAO to perform database operations.
  * The DAO should be injected into this service to access its methods.
  * Example usage:
@@ -10,7 +10,8 @@ package org.keyin.serviceplans;
  */
 public class ServicePlanService {
 
-    // Inject the ServiceDAO to access its database methods
-    // ServiceDAO dao = new ServiceDAO();
+    ServiceDAO dao = new ServiceDAO();
+
+
 
 }
