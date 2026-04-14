5fdaa128c792b266b7624507f9a57316723abc9d
Justin Seaward <jut_seaward@yahoo.com>
2026-04-14 14:41:16 -0230

    UserDAO: had to refactor ResultSet to access the right data in the user table

--- src/main/java/org/keyin/user/UserDAO.java
+++ src/main/java/org/keyin/user/UserDAO.java
@@ -7,17 +7,16 @@ import java.sql.*;
 public class UserDAO {
 
     public User getUserByUsername(String username) throws SQLException {
-        String sql = "SELECT * FROM users WHERE user_name = ?";
-//        DriverManager DatabaseConnector;
+        String sql = "SELECT * FROM users WHERE username = ?";
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, username);
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
                     return new User(
-                            rs.getInt("user_id"),
-                            rs.getString("user_name"),
-                            rs.getString("user_password"),
+                            rs.getInt("id"),
+                            rs.getString("username"),
+                            rs.getString("password_hash"),
                             rs.getString("user_role")
 
                     );
