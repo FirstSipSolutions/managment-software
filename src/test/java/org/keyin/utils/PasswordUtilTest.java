6022b013951e3e0b9d79693da020b886302f34e7
Chris <98862485+Britten66@users.noreply.github.com>
2026-04-14 19:59:29 -0300

    Add PasswordUtil unit tests

---
+++ src/test/java/org/keyin/utils/PasswordUtilTest.java
@@ -0,0 +1,14 @@
+package org.keyin.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordUtilTest {
+
+    @org.junit.jupiter.api.Test
+    void hashPassword() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkPassword() {
+    }
+}
