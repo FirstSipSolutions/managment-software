3c6ce7f52a1799437a99eb9760a5b7d5ebc11bb7
Chris <98862485+Britten66@users.noreply.github.com>
2026-04-15 18:42:32 -0300

    added starting code to hardware product class and branched off to work on these classes, tickets can be deleted now

--- src/main/java/org/keyin/hardwareproduct/HardwareProducts.java
+++ src/main/java/org/keyin/hardwareproduct/HardwareProducts.java
@@ -1,4 +1,32 @@
 package org.keyin.hardwareproduct;
 
 public class HardwareProducts {
+
+    private int itemId;
+    private String itemName;
+    private String itemType;
+    private double itemPrice;
+    private int qty_inStock;
+
+    public HardwareProducts (){}
+
+
+    // constructors below default
+    // starting with name typew price and qty in stock
+
+    public HardwareProducts(String itemName, String itemType, int itemId, int qty_inStock, double itemPrice){
+        this.itemId = itemId
+        this.itemName = itemName;
+        this.itemType = itemType;
+        this.qty_inStock = qty_inStock;
+        this.itemPrice = itemPrice;
+
+
+
+
+    }
+
+
+
+
 }
