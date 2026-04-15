f3bc07ebc1b4732d08724b13883bafc12e7e67ab
Justin Seaward <jut_seaward@yahoo.com>
2026-04-15 12:04:16 -0230

    ServicePlanService: added ArrayList getAllServicePlans

--- src/main/java/org/keyin/serviceplans/ServicePlanService.java
+++ src/main/java/org/keyin/serviceplans/ServicePlanService.java
@@ -3,6 +3,7 @@ package org.keyin.serviceplans;
 import org.keyin.customlogger.CustomLogger;
 
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Service class is responsible for handling all business logic related to service plans.
@@ -19,6 +20,10 @@ public class ServicePlanService {
 
     public void addServicePlan(ServicePlan servicePlan) throws SQLException{
             dao.addServicePlan(servicePlan);
+    }
+
+    public List<ServicePlan> getAllServicePlans() throws SQLException {
+        return dao.getAllServicePlans();
     }
 
 }
