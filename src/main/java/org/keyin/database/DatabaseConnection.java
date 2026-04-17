8570a7030b6109a26e96a7274e228c1db64de15a
Justin Seaward <jut_seaward@yahoo.com>
2026-04-17 00:48:16 -0230

    DatabaseConnection: changed the USER variable.

--- src/main/java/org/keyin/database/DatabaseConnection.java
+++ src/main/java/org/keyin/database/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
     // You only need to change the name of the database in the URL, unless PG runs on another port on your system. Default port is 5432 
     private static final String URL = "jdbc:postgresql://localhost:5432/ithelpdesksystem";
 //    By default the username is postgres and the password is whatever you set it to be. I usually keep mine simple
-    private static final String USER = "postgres";
+    private static final String USER = "admin";
     private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException, SQLException {
